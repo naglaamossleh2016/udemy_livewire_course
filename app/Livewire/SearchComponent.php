150d7e195ffe275cb09a1de51da71aa8c015cfde
naglaamossleh2016 <naglaa.mossleh@gmail.com>
2024-05-14 02:58:37 +0300

    search and querystring

--- app/Livewire/SearchComponent.php
+++ app/Livewire/SearchComponent.php
@@ -1,13 +1,15 @@
 <?php
 
 namespace App\Livewire;
-
+use App\Models\Post;
 use Livewire\Component;
 
 class SearchComponent extends Component
 {
+    public $search;
+    protected $queryString=['search'=>['except'=>'','as'=>'s']];
     public function render()
     {
-        return view('livewire.search-component');
+        return view('livewire.search-component',['posts'=>Post::where('name','like','%'.$this->search.'%')->get()]);
     }
 }
--- resources/views/livewire/search-component.blade.php
+++ resources/views/livewire/search-component.blade.php
@@ -1,3 +1,26 @@
-<div>
-    {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
+<div class="w-50 m-auto mt-5">
+    <div class="mb-5">
+        <input type="text" class="form-control" wire:model.live='search' />
+
+    </div>
+    @if ($posts->count() > 0)
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Name</th>
+                </tr>
+            </thead>
+            @foreach ($posts as $post)
+                <tbody>
+                    <tr>
+                        <th scope="row">{{ $post->id }}</th>
+                        <td>{{ $post->name }}</td>
+
+                    </tr>
+                </tbody>
+            @endforeach
+        </table>
+    @endif
+
 </div>
