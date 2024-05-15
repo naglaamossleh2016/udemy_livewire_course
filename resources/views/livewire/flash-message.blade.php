072727bf89467b800b03657d2257ecbfa728f27a
naglaamossleh2016 <naglaa.mossleh@gmail.com>
2024-05-15 04:32:27 +0300

    loading options

--- resources/views/livewire/flash-message.blade.php
+++ resources/views/livewire/flash-message.blade.php
@@ -1,7 +1,7 @@
 <div class="w-50 m-auto mt-5">
 
     <button class="btn btn-primary" wire:click="submit">Send</button>
-    <button class="btn btn-primary" wire:click="remove">remove</button>
+    <button class="btn btn-primary" wire:click="remove" wire:loading.attr="disabled">remove</button>
 
     <small class="alert alert-primary" wire:loading wire:target="remove">loading....</small>
 </div>
